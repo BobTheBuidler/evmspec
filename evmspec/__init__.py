8f1e70eaa3d6b5d75f446edd3aa9fde9f7fa861f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-08 18:32:02 +0000

    feat: new imports to main

---
+++ evmspec/__init__.py
@@ -0,0 +1,11 @@
+
+from evmspec import trace
+from evmspec.trace import FilterTrace
+
+__all__ = [
+    # modules
+    "trace",
+    
+    # structs
+    "FilterTrace",
+]
--- evmspec/trace/__init__.py
+++ evmspec/trace/__init__.py
@@ -3,3 +3,5 @@ from typing import Union
 from evmspec.trace import call, create, reward, suicide
 
 FilterTrace = Union[call.Trace, create.Trace, reward.Trace, suicide.Trace]
+
+__all__ = ["call", "create", "reward", "suicide", "FilterTrace"]
