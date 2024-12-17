76c11ae7f8f52c3e09d6acbb3dc81e2159f0b8b5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-17 05:41:38 -0400

    chore: refactor

--- evmspec/data/_main.py
+++ evmspec/data/_main.py
@@ -461,30 +461,5 @@ class TransactionHash(HexBytes32):
 class BlockHash(HexBytes32): ...
 
 
-def ttl_cache(maxsize=128, ttl=600, timer=monotonic, typed=False):
-    """Decorator to wrap a function with a memoizing callable that saves
-    up to `maxsize` results based on a Least Recently Used (LRU)
-    algorithm with a per-item time-to-live (TTL) value.
-    """
-    if maxsize is None:
-        return _cache(_UnboundTTLCache(ttl, timer), None, typed)
-    elif callable(maxsize):
-        return _cache(TTLCache(128, ttl, timer), 128, typed)(maxsize)
-    else:
-        return _cache(TTLCache(maxsize, ttl, timer), maxsize, typed)
-
-
-def _cache(cache, maxsize, typed):
-    # reimplement ttl_cache with no RLock for race conditions
-
-    def decorator(func):
-        key = keys.typedkey if typed else keys.hashkey
-        wrapper = cached(cache=cache, key=key, lock=None, info=True)(func)
-        wrapper.cache_parameters = lambda: {"maxsize": maxsize, "typed": typed}
-        return wrapper
-
-    return decorator
-
-
 __str_new__ = str.__new__
 __hb_new__ = HexBytes.__new__
