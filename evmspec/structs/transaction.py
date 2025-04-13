f2a099044f1355cce13c5de97ce95f8b760e695a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-13 03:00:10 -0400

    fix: optional yParity field for Transaction2930 (#49)

--- evmspec/structs/transaction.py
+++ evmspec/structs/transaction.py
@@ -207,6 +207,9 @@ class Transaction2930(_TransactionBase, tag="0x1", frozen=True, kw_only=True, fo
 
     type: ClassVar[HexBytes] = HexBytes("1")
 
+    yParity: Optional[uint] = UNSET
+    """The yParity for the transaction."""
+
 
 class Transaction1559(_TransactionBase, tag="0x2", frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
     """
