635cf5e6e7d1cf21305b09d1cb608d49a727c5c4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-05-03 23:39:47 +0000

    f

--- setup.py
+++ setup.py
@@ -20,7 +20,12 @@ setup(
     package_data={"evmspec": ["py.typed"]},
     include_package_data=True,
     ext_modules=mypycify(
-        ["evmspec/_utils.py", "--pretty", "--strict", "--install-types"]
+        [
+            "evmspec/_utils.py",
+            "--pretty",
+            "--install-types",
+            "--disable-error-code=unused-ignore",
+        ]
     ),
     zip_safe=False,
 )
