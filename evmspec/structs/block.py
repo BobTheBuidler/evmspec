eef5c2e7319461503ac4c59aef876cd6aac476b0
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-30 13:44:08 +0000

    Update block.py

--- evmspec/structs/block.py
+++ evmspec/structs/block.py
@@ -3,7 +3,7 @@ from functools import cached_property
 from typing import Callable, Final, Optional, Tuple, Union, final
 
 from dictstruct import DictStruct, LazyDictStruct
-from hexbytes import HexBytes
+from faster_hexbytes import HexBytes
 from msgspec import UNSET, Raw, ValidationError, field
 from msgspec.json import Decoder
 
