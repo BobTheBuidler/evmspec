d3c82e8223669f076fe607e92c16708ad7e5d904
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-13 09:25:25 -0500

    Add type annotations to data tests

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -2,5 +2,5 @@ from evmspec import *  # noqa
 from evmspec.structs import *  # noqa
 
 
-def test_imports():
+def test_imports() -> None:
     """We have no tests right now but we can at least make sure imports work."""
