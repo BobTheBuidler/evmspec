cb58f033f7bf41ba4932c132b765e92ca6802233
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-14 09:36:56 -0400

    Update transaction.py

--- evmspec/structs/transaction.py
+++ evmspec/structs/transaction.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable
 from functools import cached_property
-from typing import Any, ClassVar, Final, Union, final
+from typing import Any, ClassVar, Final, TypeAlias, final
 
 from dictstruct import LazyDictStruct
 from faster_hexbytes import HexBytes
@@ -296,11 +296,5 @@ class Transaction7702(Transaction1559, tag="0x4", frozen=True, kw_only=True, for
         return _decode_authorization_list(self._authorizationList)
 
 
-Transaction = Union[
-    TransactionLegacy,
-    Transaction2930,
-    Transaction1559,
-    Transaction4844,
-    Transaction7702,
-]
-AnyTransaction = Union[Transaction, TransactionRLP]
+Transaction: TypeAlias = TransactionLegacy | Transaction2930 | Transaction1559 | Transaction4844 | Transaction7702
+AnyTransaction: TypeAlias = Transaction | TransactionRLP
