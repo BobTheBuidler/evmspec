e9fc6daaa3b7f438aeb5e6f91ea0e9515623ffe4
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-19 16:03:39 -0400

    Update test_address_benchmarks.py

--- benchmarks/test_address_benchmarks.py
+++ benchmarks/test_address_benchmarks.py
@@ -10,10 +10,10 @@ from evmspec.data import Address
 @pytest.mark.benchmark(group="address_checksum")
 @pytest.mark.parametrize("address", ADDRESS_CASES, ids=ADDRESS_CASE_IDS)
 def test_address_checksum(benchmark: BenchmarkFixture, address: str) -> None:
-    benchmark(batch, 100, Address.checksum, address)
+    benchmark(batch, 1000, Address.checksum, address)
 
 
 @pytest.mark.benchmark(group="address_construct")
 @pytest.mark.parametrize("address", ADDRESS_CASES, ids=ADDRESS_CASE_IDS)
 def test_address_construct(benchmark: BenchmarkFixture, address: str) -> None:
-    benchmark(batch, 100, Address, address)
+    benchmark(batch, 1000, Address, address)
